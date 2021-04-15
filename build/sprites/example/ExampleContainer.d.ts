6201d0f7cd3ba36648a13ccc906ff3e5b2488049
Rodion <www.rodionkochetkow@gmail.com>
2021-04-15 10:51:58 +0300

    fix: add build to gitignore

--- build/sprites/example/ExampleContainer.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="phaser" />
-export default class ExampleContainer extends Phaser.GameObjects.Container {
-    sprite: Phaser.GameObjects.Sprite;
-    textObject: Phaser.GameObjects.Text;
-    answer: number;
-    ObjectTextureNumber: number;
-    id: number;
-    constructor(scene: Phaser.Scene, x: number, y: number);
-}
